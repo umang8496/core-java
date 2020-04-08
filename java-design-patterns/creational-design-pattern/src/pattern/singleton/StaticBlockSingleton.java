a8501be4cf3e34358d48994650bc97f40ff5f245
umang8496 <umang8april@gmail.com>
2020-04-08 11:23:49 +0530

    Singleton design pattern

---
+++ java-design-patterns/creational-design-pattern/src/pattern/singleton/StaticBlockSingleton.java
@@ -0,0 +1,26 @@
+package pattern.singleton;
+
+public class StaticBlockSingleton {
+	private static StaticBlockSingleton instance = null;
+	
+	private StaticBlockSingleton() {}
+	
+	static {
+		try {
+			instance = new StaticBlockSingleton();
+		} catch(Exception e) {
+			throw new RuntimeException("Exception occured in creating singleton instance");
+		}
+	}
+	
+	public static StaticBlockSingleton getInstance() {
+		return instance;
+	}
+}
+
+/*
+ * Static block initialization implementation is similar to eager initialization, 
+ * except that instance of class is created in the static block that provides option for exception handling.
+ * 
+ * */
+
