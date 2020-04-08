82d9c98c0be06231e3610b2c10df6437aa9f23fa
umang8496 <umang8april@gmail.com>
2020-04-08 11:22:00 +0530

    Builder design pattern

---
+++ java-design-patterns/creational-design-pattern/src/pattern/builder/Car.java
@@ -0,0 +1,21 @@
+package pattern.builder;
+
+public class Car {
+	
+	public Car() {
+		super();
+	}
+	
+	public void displayVehicleConfiguration() {
+		System.out.println("Vehicle Configuration");
+	}
+
+	public void displayVehiclePrice() {
+		System.out.println("Vehicle Price");
+	}
+
+	public void displayFuelVariant() {
+		System.out.println("Vehicle Fuel Variant");
+	}
+
+}
